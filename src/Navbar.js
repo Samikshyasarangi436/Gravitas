5ca4933241f244e8059f9c5d2b942204cb8a4113
samikshyasarangi436 <samikshyasarangi8@gmail.com>
2021-08-10 23:39:41 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+// This is just a function that returns a jsx component which is exported at the bottom
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            
+        </nav>
+
+    );
+}
+ 
+export default Navbar ;
